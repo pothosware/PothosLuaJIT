e800042b96dbce47451d4aa767959936dd112110
Nicholas Corgan <n.corgan@gmail.com>
2020-12-20 09:39:07 -0600

    Initial ConfLoader implementation

---
+++ LuaJITConfLoader.cpp
@@ -0,0 +1,168 @@
+// Copyright (c) 2020 Nicholas Corgan
+// SPDX-License-Identifier: MIT
+
+#include <Pothos/Callable.hpp>
+#include <Pothos/Exception.hpp>
+#include <Pothos/Framework.hpp>
+#include <Pothos/Object.hpp>
+#include <Pothos/Plugin.hpp>
+#include <Pothos/Proxy.hpp>
+#include <Pothos/Util/BlockDescription.hpp>
+
+#include <Poco/File.h>
+#include <Poco/Path.h>
+#include <Poco/StringTokenizer.h>
+
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+struct FactoryArgs
+{
+    std::string factory;
+
+    std::string sourceFilepath;
+    std::string functionName;
+    std::vector<std::string> inputTypes;
+    std::vector<std::string> outputTypes;
+};
+
+static Pothos::Object opaqueLuaJITBlockFactory(
+    const FactoryArgs& factoryArgs,
+    const Pothos::Object* args,
+    const size_t numArgs)
+{
+    auto blockPlugin = Pothos::PluginRegistry::get("/blocks/blocks/luajit_block");
+
+    // The LuaJIT block takes in the input and output types, which are
+    // provided by the configuration file. Theoretically, there should
+    // be nothing extra passed in the args parameter, but incorporate
+    // them anyway.
+    Pothos::ObjectVector argsVector(args, args+numArgs);
+    argsVector.emplace_back(factoryArgs.inputTypes);
+    argsVector.emplace_back(factoryArgs.outputTypes);
+
+    auto callable = blockPlugin.getObject().extract<Pothos::Callable>();
+    auto luajitBlock = callable.opaqueCall(argsVector.data(), argsVector.size());
+
+    luajitBlock.ref<Pothos::Block*>()->setName(factoryArgs.factory);
+    luajitBlock.ref<Pothos::Block*>()->call(
+        "setSource",
+        factoryArgs.sourceFilepath,
+        factoryArgs.functionName);
+
+    return luajitBlock;
+}
+
+static inline std::vector<std::string> stringTokenizerToVector(const Poco::StringTokenizer& tokenizer)
+{
+    std::vector<std::string> stdVector;
+    std::transform(
+        tokenizer.begin(),
+        tokenizer.end(),
+        std::back_inserter(stdVector),
+        [](const std::string& str){return str;});
+
+    return stdVector;
+}
+
+static std::vector<Pothos::PluginPath> LuaJITConfLoader(const std::map<std::string, std::string>& config)
+{
+    static const auto tokOptions = Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM;
+    static const std::string tokSep(" \t");
+
+    FactoryArgs factoryArgs;
+
+    // Set by calling function
+    const auto confFilePathIter = config.find("confFilePath");
+    if(confFilePathIter == config.end()) throw Pothos::Exception("No conf filepath");
+    const auto rootDir = Poco::Path(confFilePathIter->second).makeParent();
+
+    //
+    // Factory parameters
+    //
+
+    auto factoryIter = config.find("factory");
+    if(factoryIter != config.end())
+    {
+        // This will throw if the plugin path is invalid.
+        factoryArgs.factory = Pothos::PluginPath(factoryIter->second).toString();
+    }
+    else throw Pothos::Exception("No factory");
+
+    // Policy: source must be a path
+    auto sourceIter = config.find("source");
+    if(sourceIter != config.end())
+    {
+        factoryArgs.sourceFilepath = Poco::Path(rootDir, sourceIter->second).toString();
+        if(!Poco::File(factoryArgs.sourceFilepath).exists())
+        {
+            throw Pothos::FileNotFoundException(factoryArgs.sourceFilepath);
+        }
+    }
+    else throw Pothos::Exception("No source");
+
+    auto functionNameIter = config.find("function");
+    if(functionNameIter != config.end()) factoryArgs.functionName = functionNameIter->second;
+    else throw Pothos::Exception("No function name");
+
+    auto inputTypesIter = config.find("input_types");
+    if(inputTypesIter != config.end())
+    {
+        factoryArgs.inputTypes = stringTokenizerToVector(Poco::StringTokenizer(inputTypesIter->second, tokSep, tokOptions));
+    }
+    else throw Pothos::Exception("No input types");
+
+    auto outputTypesIter = config.find("output_types");
+    if(outputTypesIter != config.end())
+    {
+        factoryArgs.outputTypes = stringTokenizerToVector(Poco::StringTokenizer(outputTypesIter->second, tokSep, tokOptions));
+    }
+    else throw Pothos::Exception("No output types");
+
+    // If the doc source isn't specified, use the source itself.
+    std::string docSourceFilepath;
+    auto docSourceIter = config.find("doc_source");
+    if(docSourceIter != config.end())
+    {
+        docSourceFilepath = Poco::Path(rootDir, sourceIter->second).toString();
+        if(!Poco::File(docSourceFilepath).exists())
+        {
+            throw Pothos::FileNotFoundException(docSourceFilepath);
+        }
+
+    }
+    else docSourceFilepath = factoryArgs.sourceFilepath;
+
+    Pothos::Util::BlockDescriptionParser parser;
+    parser.feedFilePath(docSourceFilepath);
+
+    //
+    // Register all factory paths, using the parameters from the config file.
+    //
+    auto blockFactory = Pothos::Callable(&opaqueLuaJITBlockFactory)
+                            .bind(factoryArgs, 0);
+    Pothos::PluginRegistry::addCall(
+        "/blocks"+factoryArgs.factory,
+        blockFactory);
+    Pothos::PluginRegistry::add(
+        "/blocks/docs"+factoryArgs.factory,
+        parser.getJSONObject(factoryArgs.factory));
+
+    return
+    {
+        "/blocks"+factoryArgs.factory,
+        "/blocks/docs"+factoryArgs.factory
+    };
+}
+
+//
+// Register conf loader
+//
+pothos_static_block(pothosRegisterLuaJITConfLoader)
+{
+    Pothos::PluginRegistry::addCall(
+        "/framework/conf_loader/luajit",
+        &LuaJITConfLoader);
+}
